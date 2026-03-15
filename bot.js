3ad88665361fbf1dabc77ab4cdaa380ec8749f53
SealHotels <seal.mruthyunjay@gmail.com>
2026-03-15 12:54:52 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ const qrcode = require('qrcode-terminal');
   }
 });
 client.on('qr', qr => {
-  qrcode.generate(qr, { small: true });
+  qrcode.generate(qr, { small: false });
 });
 
 client.on('ready', () => {
