db2bf7c2a9d74dc51c0d614d6f2e5f4d12fdc335
SealHotels <seal.mruthyunjay@gmail.com>
2026-03-15 13:04:56 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
  const client = new Client({
+  authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
     args: [
